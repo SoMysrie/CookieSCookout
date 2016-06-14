689c9adbb425e29d7238893eba4d21300c0d13e4
SoMysrie <rethores@gmail.com>
2016-06-14 19:21:46 +0200

    Mise à jour du fichier de la bdd

--- src/sql/bdd.sql
+++ src/sql/bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 28 Avril 2016 à 23:50
+-- Généré le :  Mar 14 Juin 2016 à 19:20
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -23,13 +23,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `SiteWeb` (
   `img` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `contents` text NOT NULL
+  `recipe` text NOT NULL,
+  `ingredients` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `SiteWeb`
 --
 
-INSERT INTO `SiteWeb` (`img`, `title`, `contents`) VALUES
-('/Site/public/img/cooking/cooking_1.png', 'Riz furikake revisité par Sôma', '7 ailes de poulet + 4 oeufs + riz'),
-('/Site/public/img/cooking/cooking_2.png', 'Rôti en lamelle de boeuf de Sôma', '7 pommes de terre + 1 boeuf');
+INSERT INTO `SiteWeb` (`img`, `title`, `recipe`, `ingredients`) VALUES
+('/Site/public/img/cooking/cooking_1.png', 'Riz furikake revisité par Sôma', 'Tremper les ailes de poulet dans les oeufs et faire cuire le riz', '7 ailes de poulet + 4 oeufs + riz'),
+('/Site/public/img/cooking/cooking_2.png', 'Rôti en lamelle de boeuf de Sôma', 'Mettre le boeuf au four et cuire les pommes de terre', '7 pommes de terre + 1 boeuf');
