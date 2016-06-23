128697dc76ec62e4cc212b056d5b3d91757a8b39
ScNicolas <sirac.nicolas@orange.fr>
2016-06-23 11:35:22 +0200

    Modif Recipes

--- src/link/NotedRecipes.java
+++ src/link/NotedRecipes.java
@@ -4,7 +4,9 @@ package link;
  * Created by Nicolas_Travail on 17/03/2016.
  */
 public class NotedRecipes {
-    private String url;
+    
+	private String name;
+	private String url;
     private int vote;
     private int note;
     private String[] ingredients;
@@ -34,12 +36,28 @@ public class NotedRecipes {
         this.ingredients = ingredients;
     }
 
+    public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
     public NotedRecipes(String url){
         this.url=url;
         this.vote=0;
     }
+    public NotedRecipes(String name,String url, int vote, int note, String[] ingredients,
+			String recipe) {
+		super();
+		this.name= name;
+		this.url = url;
+		this.vote = vote;
+		this.note = note;
+		this.ingredients = ingredients;
+		this.recipe = recipe;
+	}
     public String getUrl() {
         return url;
     }
