ed931c032a6e51dfd0423e718f79f03d7f4d0871
SoMysrie <rethores@gmail.com>
2016-07-05 11:23:15 +0200

    Bouton submit de l'onglet File permet l'enregistrement dans la BDD

---
+++ src/JavaFXApp/src/application/Settings.java
@@ -0,0 +1,17 @@
+package application;
+
+/**
+ * @author SoMysrie
+ * 
+ * All application settings
+ * 
+ * Final class because it must not be inherited
+ */
+public final class Settings
+{
+	/**
+	 * Web-Service settings
+	 * */
+	public final static String WSHOST = "127.0.0.1" ; // Only local host
+	public final static int    WSPORT = 1337        ; // Port
+} ;
