7b6cdba76a948ff35585f8016c446072494c735b
danterayzen <sigerpreston@hotmail.fr>
2016-07-23 20:35:54 +0200

    Ajout du dossier + fichier du parseur d'annotation

---
+++ src/annotationParser/Todos.java
@@ -0,0 +1,13 @@
+package annotationParser;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Inherited
+public @interface Todos {
+   Todo[] value();
+}
