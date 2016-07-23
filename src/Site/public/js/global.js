602e01a4a8ff038960c425b0d8a010623e18406f
SoMysrie <rethores@gmail.com>
2016-07-23 18:44:36 +0200

    IHM recherche du site internet

--- src/Site/public/js/global.js
+++ src/Site/public/js/global.js
@@ -24,12 +24,53 @@ jQuery( document ).ready(function( $ ){
 	}) ;
 
 	// Evenement qui se déclenche lors de l'envoi d'un email
-	socket.on('sendemail', function( data ) {
+	socket.on('sendmail', function( data ) {
 		if( data.status == 'success' )
 			// Retourne un message de succés et vide les champs du formulaire
-			return toastr.success( "Votre message à bien été expédié !" ) && $( 'form#contactform button[type=reset]' ).click() ;
+			return toastr.success( "Votre message a bien été expédié !" ) && $( 'form#contactform button[type=reset]' ).click() ;
 		
-		toastr.error( "Une erreur est survenue lors de l'expédition de votre message ! Merci de bien vouloir réitéré ultérieurement." ) ;
+		toastr.error( "Une erreur est survenue lors de l'expédition de votre message ! Merci de bien vouloir réitérer ultérieurement." ) ;
+	}) ;
+
+	// Evenement qui se déclenche lors de l'envoi de la recherche
+	socket.on('sendsearch', function( data ) {
+
+		// Pour vider le résultat de la précédente recherche
+		$( 'div#searchresult' ).html('') ;
+
+		// Retourne un message d'erreur 
+		if( data.status || typeof data.length == 'undefined' )
+			return toastr.error( "Une erreur est survenue lors de votre recherche ! Merci de bien vouloir réitérer ultérieurement." ) ;
+	
+		// Affiche le nombre de recette trouvée
+		toastr.info( data.length + " recette(s) trouvée(s)" ) ;
+
+		// Block représentant la recette
+		var recipe = "<div class='recipe list'>{{titleRecipe}}<div class='thumbs'></div><div>{{nameIngredient}}<br />{{contentRecipe}}</div></div>" ;
+
+		// Parcoure toutes les recettes trouvées
+		for( var i = 0 ; i < data.length ; i++ )
+		{
+			var ingr = "<ul>" ;
+
+			// Parcoure la liste des ingrédients pour la mettre sous forme de liste
+			for( var x = 0 ; x < data[i]['nameIngredient'].length ; x++ )
+				ingr += "<li>" + ( $('input[type=radio][name=searchcdt]:checked').val() == 'recipe' ? data[i]['nameIngredient'][x] : data[i]['nameIngredient'][x].replace( $.trim( $( 'input#search' ).val() ) , '<i style="color:red">' + $.trim( $( 'input#search' ).val() ) + '</i>' ) ) + "</li>" ;
+
+			ingr += "</ul>" ;
+
+			// On ajoute la recette au résultat avec ses informations
+			$( 'div#searchresult' )
+				.append(
+					recipe
+						.replace( '{{titleRecipe}}'    , ( $('input[type=radio][name=searchcdt]:checked').val() == 'recipe' ? data[i]['titleRecipe'].replace( $.trim( $( 'input#search' ).val() ) , '<i style="color:red">' + $.trim( $( 'input#search' ).val() ) + '</i>' ) : data[i]['titleRecipe'] ) ) //
+						.replace( '{{nameIngredient}}' , ingr ) //
+						.replace( '{{contentRecipe}}'  , data[i]['contentRecipe'] ) //
+				) ;
+
+			// Spécifie l'image à afficher pour la recette courante
+			$( 'div#searchresult div.recipe.list:last-child div.thumbs' ).css( 'background-image' , "url('" + data[i]['imgRecipe'] + "')" ) ;
+		} ;
 	}) ;
 
 	// On boucle sur toutes les miniatures de recettes pour afficher la vignette 
@@ -69,5 +110,25 @@ jQuery( document ).ready(function( $ ){
 
 		// Signale que le message est en cours d'expédition
 		toastr.info( "Votre message est en cours d'expédition..." ) ;
+	}) ;
+
+	//
+	$('form#searchform').submit( function(event){
+
+		// Empêche la validation du formulaire
+		event.preventDefault() ;
+
+		// Vérifie que le champ recherche n'est pas nul
+		if( $.trim( $( 'input#search' ).val() ).length < 3 )
+			return toastr.error( "Merci de bien vouloir spécifier votre recherche !" ) ;
+
+		// Envoie des données de la recherche au serveur
+		socket.emit( 'sendsearch' , {
+			search: $.trim( $( 'input#search' ).val() ) ,
+			recipe: $('input[type=radio][name=searchcdt]:checked').val() == 'recipe'
+		}) ;
+
+		// Signale que la demande est en cours
+		toastr.info( "Recherche en cours..." ) ;
 	}) ;
 }) ;
